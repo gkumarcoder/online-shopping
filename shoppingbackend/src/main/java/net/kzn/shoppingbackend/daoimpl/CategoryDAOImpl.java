2adcd614bb87cab01a1a1d1e19ced56e539fce06
Gaurav Kumar <gkumar4@outlook.com>
2019-02-18 01:02:44 +0530

    Update CategoryDAOImpl.java

--- shoppingbackend/src/main/java/net/kzn/shoppingbackend/daoimpl/CategoryDAOImpl.java
+++ shoppingbackend/src/main/java/net/kzn/shoppingbackend/daoimpl/CategoryDAOImpl.java
@@ -21,7 +21,6 @@ public class CategoryDAOImpl implements CategoryDAO {
 	@Override
 	public List<Category> list() {
 		
-		
 		String selectActiveCategory = "FROM Category WHERE active = :active";
 		
 		Query query = sessionFactory.getCurrentSession().createQuery(selectActiveCategory);
