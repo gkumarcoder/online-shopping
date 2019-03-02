406e7f966d25b0adc1b6467721f02b24a01cfaba
Gaurav Kumar <gkumar4@outlook.com>
2019-03-02 21:56:52 +0530

    Update CategoryDAO.java

--- shoppingbackend/src/main/java/net/kzn/shoppingbackend/dao/CategoryDAO.java
+++ shoppingbackend/src/main/java/net/kzn/shoppingbackend/dao/CategoryDAO.java
@@ -4,8 +4,7 @@ import java.util.List;
 
 import net.kzn.shoppingbackend.dto.Category;
 
-public interface CategoryDAO {
-
+public interface CategoryDAO 
 	
 	Category get(int id);
 	List<Category> list();
